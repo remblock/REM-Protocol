62bffd3eb4493f4a9327efabded12bc68029590f
SSDx <60996729+ssdx-remblock@users.noreply.github.com>
2020-09-07 12:20:27 +0930

    Update rem-restore-snapshot.sh

--- rem-restore-snapshot.sh
+++ rem-restore-snapshot.sh
@@ -70,6 +70,7 @@ mv /root/root/data/snapshots/*.bin $snapshots_folder/
 bin_file=$snapshots_folder/*.bin
 echo ""
 echo "Uncompressed $latest_snapshot"
+echo ""
 rm -rf $blocks_folder
 rm -rf $state_folder
 cd ~
